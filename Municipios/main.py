56918a4bfd6215783d8080d6f37d062f9f4e5166
DESKTOP-GNJLD58\Usuario <dani.ballesterosponcet@gmail.com>
2022-01-20 16:24:20 +0000

    update

--- Municipios/main.py
+++ Municipios/main.py
@@ -1,7 +1,8 @@
 import requests as req
 import csv
 import json
-# from os import system
+from funcs import *
+from os import system
 
 # Direccion url de basa de datos en formato .csv ((NORMALEMENTE TRABAJAREMOS EN SOBRE FORMATOS .json))
 
@@ -9,65 +10,6 @@ import json
 
 # res = req.get(url).content # Se asigna una variable que devolverá/responderá(res) un contenido (Str) codificado en binario y no admite UTF8
 # res = res.decode("utf8", errors = "replace") #NOS DEVUELVE UNA STRING Se decodifica la Str para poder leerla y se reemplazan los caracteres especiales que no reconoce de UTF8 por ?
-
-
-
-
-def get_by_ine(dataset, ine_code):
-    result = None
-    for mun in dataset:
-        if mun[2] == ine_code:
-            result = mun
-    return result
-
-def get_largest(dataset):
-    current_area = 0
-    largest_mun = None
-    for mun in dataset:
-        if float(mun[-2]) > float(current_area):
-            current_area = mun[-2]
-            largest_mun = mun
-    return largest_mun
-
-def sup_total(dataset):
-    area_counter = 0
-    for mun in dataset:   
-        area_counter += float(mun[-2])
-    return round(area_counter)
-
-def densidad_total(dataset):
-    den_counter = 0
-    for mun in dataset:
-        den_counter += float(mun[-1])
-    return round(den_counter)
-
-def pob_total(dataset):
-    result = []
-    for mun in dataset:
-        result.append(float(mun[-2])*float(mun[-1]))
-    return round(sum(result))
-
-def ley_bendford(dataset):
-    densities = [mun[-1] for mun in dataset]
-    result = {}
-    for num in range(1,10):
-        result[str(num)] = 0
-    for density in densities:
-        result[density[0]] += 1/len(densities)
-    for k, v in result.items():
-        print(f"{k}: {v}")
-    
-
-def menu():
-    print("\n"*5 + " Bienvenido a la base de datos de los Municipios de la CAM ".center(150, "*")+ "\n")
-    print("1. Buscar por INE del Municipio: " + (" " * (150 - len("1. Buscar por INE del Municipio: "))) + "#" + "\n")
-    print("2. Cuál es el Municipio más grande de la CAM?"+ (" " * (150 - len("2. Cuál es el Municipio más grande de la CAM?"))) + "#" + "\n")
-    print("3. Sepa cuál es la Superficie de la CAM"+ (" " *(150 - len("3. Sepa cuál es la Superficie de la CAM"))) + "#" + "\n")
-    print("4. Sepa la densidad de población por km2 de la CAM"+ (" " *(150 - len("4. Sepa la densidad de población de la CAM"))) + "#" + "\n")
-    print("5. Población total de la CAM"+ (" " *(150 - len("5. Población total de la CAM"))) + "#" + "\n")
-    print("6. Sepa la población media por Municipio de la CAM"+ (" " *(150 - len("6. Sepa la población media por Municipio de la CAM"))) + "#" + "\n")
-    print("7. Cómo aplica la Ley de Bendford en la CAM"+ (" " * (150 - len("7. Cómo aplica la Ley de Bendford en la CAM"))) + "#" + "\n")
-    print("Q. Pulsa Q para salir" + (" " * (150 - len("Q. Pulsa Q para salir"))) + "#" + "\n")
 
 
 with open("data.csv", mode = "r", encoding= "utf8")as file:
@@ -82,45 +24,59 @@ while user != "q":
     user = input(": ")
     if user == "1":
         
-        ine_code_chosen = input("Introduzca el INE: "+ "\n"*2)
+        ine_code_chosen = input("\nIntroduzca el INE: "+ "\n"*2)
         mun_found = get_by_ine(data, ine_code_chosen)
         if mun_found:
             print("\n", f" Has escogido el Municipio {mun_found[1]} con un área de {mun_found[-2]} km2 y una densidad de {mun_found[-1]} personas/km2 ".center(100,"-"))   
         else:
             print("No se ha encontrado el Municipio")
         input("...")
-        
+        system("clear")
 
     elif user == "2":
         largest_mun = get_largest(data)
         print("\n", f" El Municipio más grande es {largest_mun[1]}con un área de {largest_mun[-2]} km2 ".center(100,"-"))
- 
+        input("...")
+        system("clear")
     
     elif user == "3":
         total_area = sup_total(data)
         print("\n", f" La Superficie total de la CAM es {total_area} km2 ".center(100,"-"))
-   
+        input("...")
+        system("clear")
+
     elif user == "4":
         densidad_total_area= densidad_total(data)
         print("\n", f" En la CAM viven {densidad_total_area} personas por km2 ".center(100, "-"))
-    
+        input("...")
+        system("clear")
+
     elif user == "5":
         poblacion_total =  pob_total(data)
         print("\n", f" La población total de la CAM es {poblacion_total} ".center(100, "-"))
-   
+        input("...")
+        system("clear")
+
     elif user == "6":
         pob_media = pob_total(data) / len(data)
         print("\n", f" La población media de la CAM es de {round(pob_media)} personas por Municipio ".center(100, "-"))
+        input("...")
+        system("clear")
 
     elif user == "7":
         ley_bendford(data)
+        input("...")
+        system("clear")
 
     elif user == "q":
         print("\n" *2, " Gracias por visitarnos ".center(155, "*"))
-        user = "q"
-    
+        input("...")
+        system("clear")
+        
+    else:
+        print("\n", " Por favor escoja una de las opciones disponibles ".center(100, "/"))
 user = "0"
-# system("cls")
+system("clear")
 
 
 
@@ -133,7 +89,7 @@ user = "0"
     
 """
 
-with open("data.csv", mode = "r", encoding= "utf8")as file:
+with open("./data.csv", mode = "r", encoding= "utf8")as file:
     data = list(csv.reader(file, delimiter=";"))
 
 
@@ -153,7 +109,7 @@ def to_json(dataset):
 
 
 
-with open("data.json", mode = "w", encoding= "utf8")as file:
+with open("./data.json", mode = "w", encoding= "utf8")as file:
     data_to_json = to_json(data)
     json.dump(data_to_json, file, indent=4, ensure_ascii=False)
 
